a43dd14f6d5c3230a9c8f1bfbac0a3160f3a9e05
JaimeBR-ui <bambas@knights.ucf.edu>
2019-08-31 00:36:47 -0400

    syntax corrections

--- src/opcontrol.cpp
+++ src/opcontrol.cpp
@@ -14,59 +14,53 @@
  * task, not resume it from where it left off.
  */
 
- //NONE OF THIS HAS BEEN TESTED BEFORE SO THERE MAY BE A LOT OF DEBUGGING THAT
- //NEEDS TO HAPPEN.
+ // Note: only the test_pneumatic() function has been tested
 
 pros::Controller master(pros::E_CONTROLLER_MASTER);
 bool state = LOW;
 pros::ADIDigitalOut pneumatic_A = pros::ADIDigitalOut('A', state);
 
-void testPneumatic(void);
-void pneumaticLift(void);
+void test_pneumatic(void);
+void pneumatic_lift(void);
 
-void opcontrol() {
-	while (true) {
-		testPneumatic();
+void opcontrol()
+{
+	while (true)
+	{
+		test_pneumatic();
 		//using single action pneumatics for a lift
-		//pneumaticLift();
+		//pneumatic_lift();
+		pros::delay(20);
 	}
-	return;
 }
 
-void testPineumatic(void){
-	if(master.get_digital(DIGITAL_A)){
+void test_pneumatic(void)
+{
+	if (master.get_digital(DIGITAL_A))
 		state = HIGH;
-	}
 	else
 		state = LOW;
 	pneumatic_A.set_value(state);
-	pros::delay(20);
-	return;
 }
 
-void pneumaticLift(void){
-	static int liftWasMoving = 1;
-	if(master.get_digital(DIGITAL_L1)){//moving up
+void pneumatic_lift(void)
+{
+	static int lift_was_moving = 1; // values: -1, 0, 1
+	if (master.get_digital(DIGITAL_L1))
+	{ // moving up
 		pneumatic_A.set_value(HIGH);
-		//code to make lift go up
+		// code to make lift go up
 	}
-	else if(master.get_digital(DIGITAL_L2)){//moving down
+	else if (master.get_digital(DIGITAL_L2))
+	{ // moving down
 		pneumatic_A.set_value(LOW);
-		//code to make the lift go down
-	}else{
-		//set value HIGH or LOW depending on wether or not the PID is heating up the
+		// code to make the lift go down
+	}
+	else
+	{
+		// set value HIGH or LOW depending on wether or not the PID is heating up the
 		// motors too much
 		pneumatic_A.set_value(LOW);
-		//send lift to hold position
+		// send lift to hold position
 	}
-	return;
 }
-
-/*
-pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-int left = master.get_analog(ANALOG_LEFT_Y);
-int right = master.get_analog(ANALOG_RIGHT_Y);
-pros::delay(20);
-*/
